5cc7e94236ba85efe7cc331c61a8f5ad92e150c2
Masum <hydrapikachu29@gmail.com>
2025-06-26 12:48:18 +0530

    Added box properties

--- app/src/main/java/com/masum/calculatorbasic/Buttons.kt
+++ app/src/main/java/com/masum/calculatorbasic/Buttons.kt
@@ -1,7 +1,12 @@
 package com.masum.calculatorbasic
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 
 @Composable
 fun Buttons (
@@ -9,5 +14,13 @@ fun Buttons (
     modifier: Modifier,
     onClick: () -> Unit
 ) {
+    Box (
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .clip(CircleShape)
+            .clickable { onClick () }
+            .then(modifier)
+    ){
 
+    }
 }
