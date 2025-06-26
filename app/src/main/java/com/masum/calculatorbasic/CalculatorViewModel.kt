851e9fb2a7e613483b837c68d806274e621e082e
Masum <hydrapikachu29@gmail.com>
2025-06-26 19:28:31 +0530

    Added basic viewmodel config

---
+++ app/src/main/java/com/masum/calculatorbasic/CalculatorViewModel.kt
@@ -0,0 +1,10 @@
+package com.masum.calculatorbasic
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class CalculatorViewModel: ViewModel() {
+    var state by mutableStateOf(States())
+}
