dedea21f81faf069947bb1bb07e20b98ddc74c1e
Masum <hydrapikachu29@gmail.com>
2025-06-26 20:46:04 +0530

    Added all the properties of the calculator display

--- app/src/main/java/com/masum/calculatorbasic/CalculatorScreen.kt
+++ app/src/main/java/com/masum/calculatorbasic/CalculatorScreen.kt
@@ -5,10 +5,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun CalculatorScreen(
@@ -24,7 +31,16 @@ fun CalculatorScreen(
                 .align(Alignment.BottomCenter),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-
+            Text(
+                text = states.number1 + (states.operation?.symbol?: "") + states.number2,
+                textAlign = TextAlign.End,
+                modifier = Modifier.fillMaxWidth()
+                    .padding(16.dp),
+                fontSize = 80.sp,
+                fontStyle = FontStyle.Normal,
+                color = Color.White,
+                maxLines = 2
+            )
         }
     }
 }
