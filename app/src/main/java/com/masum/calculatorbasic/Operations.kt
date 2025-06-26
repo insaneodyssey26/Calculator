199cd071b506de388cf065a0904f43410b237241
Masum <hydrapikachu29@gmail.com>
2025-06-26 14:24:23 +0530

    Added operations

--- app/src/main/java/com/masum/calculatorbasic/Operations.kt
+++ app/src/main/java/com/masum/calculatorbasic/Operations.kt
@@ -1,5 +1,8 @@
 package com.masum.calculatorbasic
 
-sealed class Operations {
-
+sealed class Operations (val symbol: String){
+    object Add: Operations("+")
+    object Subtract: Operations("-")
+    object Multiply: Operations("X")
+    object Divide: Operations("/")
 }
