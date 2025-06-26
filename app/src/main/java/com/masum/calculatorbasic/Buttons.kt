c8ff5ab692ac28c5f5e5c2e8f2bf4e083933f022
Masum <hydrapikachu29@gmail.com>
2025-06-26 12:59:05 +0530

    Added text properties for buttons

--- app/src/main/java/com/masum/calculatorbasic/Buttons.kt
+++ app/src/main/java/com/masum/calculatorbasic/Buttons.kt
@@ -3,10 +3,13 @@ package com.masum.calculatorbasic
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Buttons (
@@ -21,6 +24,10 @@ fun Buttons (
             .clickable { onClick () }
             .then(modifier)
     ){
-
+        Text(
+            text = symbol,
+            fontSize = 36.sp,
+            color = MaterialTheme.colorScheme.onBackground
+        )
     }
 }
