c3c17492a6b6642db4a502d6bc2178f620e7c733
Masum <hydrapikachu29@gmail.com>
2025-06-27 18:45:32 +0530

    Added required actions

--- app/src/main/java/com/masum/calculatorbasic/CalculatorViewModel.kt
+++ app/src/main/java/com/masum/calculatorbasic/CalculatorViewModel.kt
@@ -1,5 +1,6 @@
 package com.masum.calculatorbasic
 
+import android.service.credentials.Action
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -8,4 +9,14 @@ import androidx.lifecycle.ViewModel
 class CalculatorViewModel: ViewModel() {
     var state by mutableStateOf(States())
     private set
+
+    fun onAction (action: Actions) {
+        when (action) {
+            is Actions.Number -> enterNumber(action.number)
+            is Actions.Operation -> enterOperation(action.operation)
+            Actions.Clear -> clear()
+            Actions.Calculate -> calculate()
+            Actions.Delete -> delete()
+        }
+    }
 }
