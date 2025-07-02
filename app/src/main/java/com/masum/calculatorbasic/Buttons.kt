ad6cca31e4303503313b97e028787b4127ce1f83
Masum <hydrapikachu29@gmail.com>
2025-07-03 01:28:33 +0530

    Removed unused imports from Screen.kt

--- app/src/main/java/com/masum/calculatorbasic/Buttons.kt
+++ app/src/main/java/com/masum/calculatorbasic/Buttons.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -21,7 +22,9 @@ fun Buttons (
         contentAlignment = Alignment.Center,
         modifier = Modifier
             .clip(CircleShape)
-            .clickable { onClick () }
+            .clickable {
+                onClick ()
+            }
             .then(modifier)
     ){
         Text(
--- app/src/main/java/com/masum/calculatorbasic/CalculatorScreen.kt
+++ app/src/main/java/com/masum/calculatorbasic/CalculatorScreen.kt
@@ -1,17 +1,13 @@
 package com.masum.calculatorbasic
 
-import android.service.credentials.Action
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
